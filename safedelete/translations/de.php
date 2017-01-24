1a45636ad702e6f8485c06c2195e3aa66a8dbf52
Christian Ruhstaller <christian.ruhstaller@goldinteractive.ch>
2017-01-24 14:08:58 +0100

    translations

--- safedelete/translations/de.php
+++ safedelete/translations/de.php
@@ -1,11 +1,12 @@
 <?php
 
 return [
-    'Element is still referenced' => 'Element is still referenced',
-    'The selected element is still in use and can not be deleted just like that. Check the information below and decide wisely.' => 'The selected element is still in use and can not be deleted just like that. Check the information below and decide wisely.',
+    'Element is still referenced' => 'Element wird verwendet',
+    'The selected element is still in use and can not be deleted just like that.' => 'Das Element wird verwendet und kann nicht gelöscht werden.',
     'Safe Delete…' => 'Sicher löschen…',
     'Element type' => 'Elementtyp',
     'Reference element type' => 'Referenzierter Elementtyp',
     'Field' => 'Feld',
     'Source Element' => 'Quellelement',
+    'WARNING! Deletion of referenced elements can lead to unexpected consequences and errors in the frontend.' => 'WARNUNG! Das Löschen von Elementen welche verwendet werden kann zu Fehlern im Frontend führen.'
 ];
--- safedelete/translations/en.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'Element is still referenced' => 'Element is still referenced',
-    'The selected element is still in use and can not be deleted just like that. Check the information below and decide wisely.' => 'The selected element is still in use and can not be deleted just like that. Check the information below and decide wisely.',
-    'Safe Delete…' => 'Sicher löschen…',
-];
